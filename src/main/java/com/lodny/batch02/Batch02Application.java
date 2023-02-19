09d9320f005e66833c61a51f70d0311849b8fd51
Lodny <drinkjuice@naver.com>
2023-02-19 15:42:03 +0900

    init

---
+++ src/main/java/com/lodny/batch02/Batch02Application.java
@@ -0,0 +1,13 @@
+package com.lodny.batch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Batch02Application.class, args);
+    }
+
+}
